be2aa22ce504f8298532c2bcf488cb112678244b
Etienne Samson <samson.etienne@gmail.com>
2023-07-31 20:15:19 +0200

    Hack hack hack

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,21 @@
 "use strict";
 
 import clear from "rollup-plugin-clear";
-import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
+import { readFileSync } from "fs";
+import resolve from "@rollup/plugin-node-resolve";
 import screeps from "rollup-plugin-screeps";
+import typescript from "rollup-plugin-typescript2";
 
 let cfg;
 const dest = process.env.DEST;
 if (!dest) {
   console.log("No destination specified - code will be compiled but not uploaded");
-} else if ((cfg = require("./screeps.json")[dest]) == null) {
-  throw new Error("Invalid upload destination");
+} else {
+  cfg = JSON.parse(readFileSync("./screeps.json"))[dest];
+  if (!cfg) {
+    throw new Error("Invalid upload destination");
+  }
 }
 
 export default {
